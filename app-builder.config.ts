e126ee11f8e86635404386b61a7a611393eeea2b
Alexey Sudilovskiy <sudilovskiy@yandex-team.ru>
2025-01-22 19:16:35 +0100

    chore: enable monaco config

--- app-builder.config.ts
+++ app-builder.config.ts
@@ -2,6 +2,7 @@ import {type ServiceConfig, defineConfig} from '@gravity-ui/app-builder';
 
 const config = defineConfig((): ServiceConfig => ({
     client: {
+        monaco: {},
         newJsxTransform: true,
     }
 }));
